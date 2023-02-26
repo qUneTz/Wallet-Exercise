deb2f2b627489713d835331c4b45bd612442356e
Eduard <edy@local>
2023-02-27 00:15:19 +0200

    Implemented DebitWallet command DebitWallet implements some "business" logic like can't debit more than the balance of the wallet. At the end saves the state throught the repo that was injected throught the constructor

---
+++ src/domain/commands/DebitWallet.ts
@@ -0,0 +1,40 @@
+import IWalletRepository from "../model/IWalletRepository.js";
+import { WalletState, TransactionType } from "../model/WalletState.js";
+
+export default async function (
+  walletID: string,
+  amount: number,
+  transactionID: string,
+  walletRepository: IWalletRepository
+): Promise<WalletState> {
+  if (amount < 0) {
+    return Promise.reject({
+      message: "Amount cannot be negative",
+      status: 400,
+    });
+  }
+
+  const currentWalletState = await walletRepository.getWalletByID(walletID);
+
+  if (currentWalletState == undefined) {
+    return Promise.reject({ message: "Wallet not found", status: 404 });
+  }
+  if (currentWalletState.balance < amount) {
+    return Promise.reject({ message: "Insufficinet funds", status: 400 });
+  }
+  if (currentWalletState.transactionID == transactionID) {
+    return Promise.reject({ message: "Request was accepted", status: 202 });
+  }
+
+  const newWalletState = new WalletState(
+    walletID,
+    amount,
+    transactionID,
+    TransactionType.DEBIT,
+    currentWalletState.balance - amount,
+    currentWalletState.version + 1
+  );
+  await walletRepository.saveWalletState(newWalletState);
+
+  return Promise.resolve(newWalletState);
+}
