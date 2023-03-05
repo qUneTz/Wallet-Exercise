6c8617fe4d271542faf2ada90f251ced80e3201e
Eduard <edy@local>
2023-03-05 17:04:25 +0200

    Refactoring: GetWallet query; class to function

--- src/domain/queries/GetWallet.ts
+++ src/domain/queries/GetWallet.ts
@@ -1,16 +1,9 @@
 import IWalletRepository from "../model/IWalletRepository.js";
 import { WalletState } from "../model/WalletState.js";
 
-class GetWallet {
-  private walletRepository: IWalletRepository;
-
-  constructor(walletRepository: IWalletRepository) {
-    this.walletRepository = walletRepository;
-  }
-
-  async getWallet(id: string): Promise<WalletState> {
-    const wallet = await this.walletRepository.getWalletByID(id);
-    return wallet;
-  }
-}
-export default GetWallet;
+export default async (
+  id: string,
+  walletRepository: IWalletRepository
+): Promise<WalletState> => {
+  return await walletRepository.getWalletByID(id);
+};
