794986819b7609f5f50461ec48ed378c18272fff
Eduard <edy@local>
2023-03-06 02:16:31 +0200

    Definded HTTP responses

---
+++ src/infrastructure/routes/HTTP_Responses.ts
@@ -0,0 +1,8 @@
+export const HTTP_Responses = {
+  OK: { status: 200, message: "OK" },
+  CREATED: { status: 201, message: "Created" },
+  ACCEPTED: { status: 202, message: "Accepted" },
+  BAD_REQUEST: { status: 400, message: "Bad Request" },
+  NOT_FOUND: { status: 404, message: "Not Found" },
+  SERVER_ERROR: { status: 500, message: "Internal Server Error" },
+};
